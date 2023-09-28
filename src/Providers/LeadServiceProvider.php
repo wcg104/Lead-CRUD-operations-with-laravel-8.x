7c63c9220891e9dd27faf5badef3beb0e28021de
nima <nima@webcodegenie.com>
2023-09-28 18:31:51 +0530

    #WCGI-1172 update service provider

--- src/Providers/LeadServiceProvider.php
+++ src/Providers/LeadServiceProvider.php
@@ -8,18 +8,21 @@
         public function boot()
         {
          
-          $this->publishes([
-            __DIR__.'/../database/migrations/' => database_path('migrations')
-          ], 'lead');
+            $this->publishes([
+              __DIR__.'/../database/migrations/' => database_path('migrations')
+              ], 'lead');
               $this->publishes([
                 __DIR__.'/../Http/Controllers/' => app_path('Http/Controllers')
-            ], 'lead');
-            $this->publishes([
-              __DIR__.'/../Models/' => app_path('Models')
-          ], 'lead');
-          $this->publishes([
-            __DIR__.'/../Http/Requests' => app_path('Http/Requests')
-        ], 'lead');
+              ], 'lead');
+              $this->publishes([
+                __DIR__.'/../Models/' => app_path('Models')
+              ], 'lead');
+              $this->publishes([
+                __DIR__.'/../Http/Requests' => app_path('Http/Requests')
+              ], 'lead');
+              $this->publishes([
+                __DIR__.'/../test/Feature' => test_path('Feature')
+              ], 'lead');
         }
    }
 ?>
